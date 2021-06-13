72ddfd7ba551c79a44298cc27d9a93e423313192
Avi Gautam <61787292+avigautam-329@users.noreply.github.com>
2021-06-13 21:11:08 +0530

    Update Sorting

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Implement Insertion Sort.
+void insertionSort(int *arr, int n){
+	
+	for(int i=1 ; i< n; ++i){
+		int current = arr[i];
+		int j;
+		for(j = i-1 ; j > -1 ; --j){
+			if(current < arr[j]){
+				arr[j+1] = arr[j];
+			}else{
+				break;
+			}
+		}
+		arr[j+1] = current ; 
+	}
+	
+}
+
+
+int main(){
+	int n;
+	cin >> n;
+	
+	int *arr = new int[n];
+	for(int i=0;i<n;++i){
+		cin >> arr[i];
+	}
+	
+	insertionSort(arr,n);
+	
+	for(int i=0;i<n;++i){
+		cout << arr[i] << " ";
+	}
+	
+	return 0;
+}
--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 void selectionSort(int *arr, int n){
+	int min_pos = 0;
+	int current = 0;
+	
+	
+	//Goes on for only n-1 rounds.
+	for(int i=0;i<n-1;++i){
+		int min_num=INT_MAX;
+		for(int j=current;j<n;++j){
+			if(arr[j] < min_num){
+				min_num = arr[j];
+				min_pos = j;
+			}
+		}
+		int temp = arr[current];
+		arr[current] = arr[min_pos];
+		arr[min_pos] = temp;
+		
+		current++;
+	}
 	
 }
 
@@ -12,6 +31,12 @@ int main(){
 	int *arr = new int[n];
 	for(int i = 0; i < n ; ++i){
 		cin >> arr[i];
+	}
+	
+	selectionSort(arr,n);
+	
+	for(int i = 0; i < n ; ++i){
+		cout << arr[i] << " ";
 	}
 	
 	return 0;
